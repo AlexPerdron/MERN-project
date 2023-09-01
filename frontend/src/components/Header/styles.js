e54e5a3105fb42cba4fcbe45ed3ce61124a9e63c
Nguyễn Tuấn Khoa <khoanguyen030803@gmail.com>
2023-09-01 16:43:36 +0700

    Re-Structure the src in frontend folder

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -1,12 +1,10 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
     container:{
-        marginTop: theme.spacing(3),
-        marginBottom: theme.spacing(6),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: theme.palette.primary.main,
+        marginBottom: 20,
+        fontWeight: 'lighter',
+        padding: '5px 0'
     }
 }))
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 import Header from '../components/Header';
 import PostList from '../components/PostList';
-import Signin from '../components/Signin';
-import NavBar from '../components/NavBar';
 
 export default function HomePage() {
    return ( 
     <Container maxWidth='lg' className={{}}>
         <Header />
-        <NavBar />
-        <Signin />
+        <PostList />
     </Container>
     )
 }
