48fe861627948e9688c51d8f6e94c53e104e97e1
Nguyễn Tuấn Khoa <khoanguyen030803@gmail.com>
2023-09-17 17:21:54 +0700

    Add posts feature

--- frontend/src/components/PostList/index.js
+++ frontend/src/components/PostList/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Post from './Post';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../../redux/actions';
+import { postsState$ } from '../../redux/selectors';
 
 export default function PostList() {
-    return (
-        <div>
-            PostList
-        </div>
-    )
+    //Calling API to get data
+    const dispatch = useDispatch();
+    const posts = useSelector(postsState$);
+
+    console.log('[PostList - posts]', posts)
+
+    React.useEffect(() => {
+        dispatch(actions.getPosts.getPostsRequest());
+    }, [dispatch]);
+
+    //Post
+    return <Grid container spacing = {2} aignment='stretch'>
+        {posts.map(post => (
+            <Grid item xs={12} sm={6}>
+                <Post key = {post._id} post={post}/>
+            </Grid>
+        ))}
+    </Grid>;
 }
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Container, Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+
 import Header from '../components/Header';
 import PostList from '../components/PostList';
+import useStyles from './HomePageStyles';
 
 export default function HomePage() {
-   return ( 
-    <Container maxWidth='lg' className={{}}>
-        <Header />
-        <PostList />
-    </Container>
-    )
+    const classes = useStyles();
+
+    return ( 
+        <Container maxWidth='lg' >
+            <Header />
+            <PostList />
+            <Fab 
+                color ='red'className={classes.fab}>
+                <AddIcon />
+            </Fab>
+        </Container>
+        )
 }
