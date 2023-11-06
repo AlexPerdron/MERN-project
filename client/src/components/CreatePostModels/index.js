97ca24f435f1a7a79bf8afca2041adf51ce9e0c2
Nguyễn Tuấn Khoa <ITCSIU21140@student.hcmiu.edu.vn>
2023-11-06 08:29:44 +0700

    Khoa|Add|Create new post ui

--- client/src/components/CreatePostModels/index.js
+++ client/src/components/CreatePostModels/index.js
@@ -1,48 +1,69 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { IconButton, Modal, TextField, TextareaAutosize, Typography } from '@material-ui/core';
-import DoneIcon from '@material-ui/icons/Done';
-import CloseIcon from '@material-ui/icons/Close';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import FileBase64 from "react-file-base64";
+import {
+	IconButton,
+	Modal,
+	TextField,
+	TextareaAutosize,
+	Typography,
+} from "@material-ui/core";
+import DoneIcon from "@material-ui/icons/Done";
+import CloseIcon from "@material-ui/icons/Close";
 
-import { modalState$ } from '../../redux/selectors';
-import useStyles from './styles';
+import { modalState$ } from "../../redux/selectors";
+import useStyles from "./styles";
+import { hideModal } from "../../redux/actions";
 
 export default function CreatePostModels() {
-    //Calling API to get data
-    const { isShow } = useSelector(modalState$);
-    const classes = useStyles();
+	const dispatch = useDispatch();
+	//Calling API to get data
+	const { isShow } = useSelector(modalState$);
+	const classes = useStyles();
 
-    const body = (
-        <div className ={classes.popup} id ="simple-modal-title">
-            <IconButton display="flex" variant="contained" className={classes.closeButton}>
-                <CloseIcon/>
-            </IconButton>
-            <Typography display='flex' variant='h6' align='center' className={classes.typography}>
-                Create New Post
-            </Typography>
-            <form noValidate autoComplete='off' className={classes.form}>
-                <TextField
-                    className={classes.title}   
-                    required
-                    label ='Title'
-                />
-                <TextareaAutosize
-                    className={classes.textarea}
-                    maxRows={15}
-                    minRows={10}
-                    label ='Content...'
-                />
-            </form>
-            <IconButton variant="contained" className={classes.doneButton}>
-                <DoneIcon/>
-            </IconButton>
-        </div>
-    );
-    console.log({isShow});
-    return (
-        <div>
-            <Modal open ={ isShow }>{body}</Modal>
-        </div>
-    )
+	const onClose = React.useCallback(() => {
+		dispatch(hideModal());
+	}, [dispatch]);
+
+	const body = (
+		<div className={classes.popup} id="simple-modal-title">
+			<IconButton
+				display="flex"
+				variant="contained"
+				className={classes.closeButton}
+				onClick={onClose}
+			>
+				<CloseIcon />
+			</IconButton>
+			<Typography
+				display="flex"
+				variant="h6"
+				align="center"
+				className={classes.typography}
+			>
+				Create New Post
+			</Typography>
+			<form noValidate autoComplete="off" className={classes.form}>
+				<TextField className={classes.title} label="Title" />
+				<TextareaAutosize
+					className={classes.textarea}
+					maxRows={15}
+					minRows={10}
+					label="Content..."
+				/>
+				<FileBase64 accept="image/*" multiple={false} type="file"/>
+				<IconButton className={classes.doneButton}>
+					<DoneIcon />
+				</IconButton>
+			</form>
+		</div>
+	);
+	console.log({ isShow });
+	return (
+		<div>
+			<Modal open={isShow} onClose={onClose}>
+				{body}
+			</Modal>
+		</div>
+	);
 }
-
