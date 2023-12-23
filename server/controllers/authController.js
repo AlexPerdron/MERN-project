31c205f9b11893810a31031c41236afeae5afb9b
Nguyễn Tuấn Khoa <ITCSIU21140@student.hcmiu.edu.vn>
2023-12-23 09:09:24 +0700

    Khoa|Fix Bug|Complete jwt for creating user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,46 +1,38 @@
-import bodyParser from "body-parser";
+import {
+	generateJwtToken,
+} from "../middleware/jwt.js"
 import { UserModel } from "../models/UserModels.js";
-import { boolean } from "zod";
-
-const secretKey = process.env.SECRET_KEY;
-
-export const generateJwtTokenHandler = async (req, res) => {
-	const issuer = SocialMe;
-	const expiresDays = "10d";
-	const payload = { userId };
-	const token = jwt.sign(payload, secretKey, issuer, expiresDays);
-
-	return token;
-}
 
 export const createUserHandler = async (req, res) => {
 	try {
-		const {email, password, username} = req.body; 
+		const email = req.body.email;
+		const username = req.body.username;
+		const password = req.body.password;
 
 		const user = await UserModel.where({email: email}).findOne();
-		console.log(user);
+
 		if (user) {
 			res.status(404).json({ 
 				message: "User already exists",
-				user 
 			});
 		}
 		else {
-			const hashedPassword = await bcrypt.hashSync(password, 10);
-
-			await createUser({
-				username,
-				email,
-				password: hashedPassword,
+			const newUser = UserModel.create({
+				email: email,
+				username: username,
+				password: password,
 			});
-
-			res.status(200).json({ 
-				status: "success",
-				message: "User created" });
+			// await newUser.save();
+			let token = generateJwtToken(newUser._id);
+			res.status(201).json({ 
+				message: "User created successfully",
+				// token: token,
+			});
 		}
 	}
 	catch (error) {
-		res.status(500).json({ error: error });
+		res.status(500).json({ 
+			error: error });
 	}
 };
 // export const test = async (req, res) => {
